82219f98417ddb1a427cd76890dd644f0c365644
SpookyBeverage <david@chs.us>
2021-02-13 17:12:32 -0500

    Calendar Works Pogger!

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,16 +12,7 @@ rhit.PROFILE_PHOTO = "ProfilePhoto";
 rhit.USER_PROFILES = "UserProfiles"
 rhit.PROFILE_NAME = "ProfileName";
 rhit.PROFILE_TYPE= "ProfileType";
-
-// let calendar = new Calendar(calendarEl, {
-// 	plugins: [ googleCalendarPlugin ],
-// 	googleCalendarApiKey: 'AIzaSyDMBK7Vz-JbRyHdA5eJn1LGl-UiAukHBaU',
-// 	events: {
-// 	  googleCalendarId: 'fr7a99kuoek1b669l8uicfdo7k@group.calendar.google.com',
-// 	//   className: 'gcal-event' // an option!
-// 	}
-//   });
-
+rhit.CALENDAR_ID = 'fr7a99kuoek1b669l8uicfdo7k@group.calendar.google.com';
 function htmlmToElement(html){
 	var template = document.createElement('template');
 	html = html.trim();
@@ -376,13 +367,16 @@ rhit.initPage = function(){
 		document.querySelector("#userNameText").innerHTML = `Welcome, ${rhit.loginManager.uid}!`;
 		document.querySelector("#scheduleVisitBtn").onclick = () => window.location = "https://ems.rose-hulman.edu/emswebapp/";
 		document.querySelector("#recommendGameBtn").onclick = () => window.location.href = "/recommendation.html";
-		document.addEventListener('DOMContentLoaded', function() {
-			var calendarEl = document.getElementById('calendar');
-			var calendar = new FullCalendar.Calendar(calendarEl, {
-			  initialView: 'dayGridMonth'
-			});
-			calendar.render();
+		let calendarEl = document.querySelector('#calendar');
+		rhit.calendar = new FullCalendar.Calendar(calendarEl, {	
+			googleCalendarApiKey: 'AIzaSyDMBK7Vz-JbRyHdA5eJn1LGl-UiAukHBaU',
+			events: {
+			  googleCalendarId: 'fr7a99kuoek1b669l8uicfdo7k@group.calendar.google.com'
+			}
 		  });
+		console.log("rendering Calendar");
+		rhit.calendar.render();
+		
 	}
 	if(document.querySelector("#recommendationPage")){
 		console.log("You are on the recommendationPage");
@@ -443,4 +437,123 @@ rhit.main = function () {
 	});
 };
 
+
+
+
+
+
+
+
+
+//Code Recieved From 
+//https://developers.google.com/calendar/quickstart/js
+var CLIENT_ID = '452380067208-ibtvsdodmktiqrnae81thqgj7lpbfjr6.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyDMBK7Vz-JbRyHdA5eJn1LGl-UiAukHBaU';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = 'https://www.googleapis.com/auth/calendar';
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+/**
+ *  On load, called to load the auth2 library and API client library.
+ */
+function handleClientLoad() {
+  gapi.load('client:auth2', initClient);
+}
+
+/**
+ *  Initializes the API client library and sets up sign-in state
+ *  listeners.
+ */
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
+  }
+
+  /**
+   *  Initializes the API client library and sets up sign-in state
+   *  listeners.
+   */
+  function initClient() {
+	gapi.client.init({
+	  apiKey: API_KEY,
+	  clientId: CLIENT_ID,
+	  discoveryDocs: DISCOVERY_DOCS,
+	  scope: SCOPES
+	}).then(function () {
+	  // Listen for sign-in state changes.
+	  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+	  // Handle the initial sign-in state.
+	  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+	  authorizeButton.onclick = handleAuthClick;
+	  signoutButton.onclick = handleSignoutClick;
+	}, function(error) {
+	  appendPre(JSON.stringify(error, null, 2));
+	});
+  }
+
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+	  rhit.calendarSignedIn = true;
+	  authorizeButton.style.display = 'none';
+	  signoutButton.style.display = 'block';
+	  listUpcomingEvents();
+	} else {
+		rhit.calendarSignedIn = false;
+	  authorizeButton.style.display = 'block';
+	  signoutButton.style.display = 'none';
+	}
+  }
+
+  /**
+   *  Sign in the user upon button click.
+   */
+  function handleAuthClick(event) {
+	gapi.auth2.getAuthInstance().signIn();
+  }
+
+  /**
+   *  Sign out the user upon button click.
+   */
+  function handleSignoutClick(event) {
+	gapi.auth2.getAuthInstance().signOut();
+  }
+
+  /**
+   * Append a pre element to the body containing the given message
+   * as its text node. Used to display the results of the API call.
+   *
+   * @param {string} message Text to be placed in pre element.
+   */
+  function appendPre(message) {
+	var pre = document.getElementById('content');
+	var textContent = document.createTextNode(message + '\n');
+	pre.appendChild(textContent);
+  }
+
+
+
+
+
+
+
+
 rhit.main();
+
+
+
+
+
+
+
+
